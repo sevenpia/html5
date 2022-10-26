ba02b2faad8bb8a29a4e316acbd5317478f52fbe
sevenpia <sevenpia@naver.com>
2022-10-26 21:16:55 +0900

    star font

---
+++ js/script21.js
@@ -0,0 +1,13 @@
+
+let mainText = document.querySelector("h1");
+
+window.addEventListener('scroll', function () {
+  let value = window.scrollY
+  console.log("scrollY", value)
+  if (value > 250) {
+    mainText.style.animation = "reset 1s ease-out forwards";
+  } else {
+    mainText.style.animation = "move 1s ease-out";
+  }
+})
+
