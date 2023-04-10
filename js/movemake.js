2dfda701ba53202c702ea5bd54fd9dabcfedb25c
sevenpia <sevenpia@naver.com>
2023-04-10 11:12:00 +0900

    movemake

---
+++ js/movemake.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+  // (selector).animate({styles},speed,easing,callback)
+  $(".subtop3 h2").animate({ "top": "0", "opacity": "1" }, 2000, function () {
+    $(".subtop3 p.move").animate({ "bottom": "-20px", "opacity": "1" }, 1000)
+  }
+  );
+
+})  /* ready end */
+/* block commnet : shit alt a */
+
