0b9bca8fa888d51f755cde50a9b94ee0be19c0c3
sevenpia <sevenpia@naver.com>
2023-04-19 10:57:47 +0900

    shrink 2

---
+++ jQuery_16_shrinking-navbar_base-1111/js/script2.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+  /*   윈도우에 스크롤이 일어나면
+    상단 header 의 높이가    120  에서  60 으로 변경된다. 
+   */
+
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop()) {
+      $("header").addClass('shrink');
+      console.log("header ")
+    } else {
+      $("header").removeClass('shrink');
+    }
+  })
+
+}) /*   ready end  */
+
+
