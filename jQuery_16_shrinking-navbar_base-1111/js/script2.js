d5fe363eeac68aa42394c8a9126389dc4ab95c4d
sevenpia <sevenpia@naver.com>
2023-04-19 13:13:37 +0900

    sticky2 modi

--- jQuery_16_shrinking-navbar_base-1111/js/script2.js
+++ jQuery_16_shrinking-navbar_base-1111/js/script2.js
@@ -1,18 +1,35 @@
 $(document).ready(function () {
 
   /*   윈도우에 스크롤이 일어나면
-    상단 header 의 높이가    120  에서  60 으로 변경된다. 
+    상단 header 의 높이가   120  에서  60 으로 변경된다. 
+    로고가 변경된다. 
    */
 
+  var $window = $(window),
+    $header = $("header"),
+    $defalutLogo = "images/logo.svg",
+    $smallLogo = "images/logo-shrink.svg";
 
-  $(window).scroll(function () {
-    if ($(window).scrollTop()) {
-      $("header").addClass('shrink');
-    } else {
-      $("header").removeClass('shrink');
+  $window.scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      if (!$header.hasClass('shrink')) {
+        $header.addClass('shrink');
+        logoChange($smallLogo);
+      } else {
+        if ($header.hasClass('shrink')) {
+          $header.removeClass('shrink');
+          logoChange($defalutLogo);
+        }
+      }
     }
   })
 
+  // logoChage함수
+  function logoChange(newPath) {
+    var $logo = $('.logo');
+    $logo.fadeOut(300, function () {
+      $logo.attr('src', newPath);
+      $logo.fadeIn(300);
+    });
+  }
 }) /*   ready end  */
-
-
