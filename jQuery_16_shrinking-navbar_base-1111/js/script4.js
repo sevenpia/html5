4da2bca63bcda7c437082b76fe0c2db050a4df25
sevenpia <sevenpia@naver.com>
2023-04-20 15:08:57 +0900

    shrink4 modi

--- jQuery_16_shrinking-navbar_base-1111/js/script4.js
+++ jQuery_16_shrinking-navbar_base-1111/js/script4.js
@@ -11,16 +11,19 @@ $(document).ready(function () {
     $mainHeader = $("#main-header"),
     $defaultLogo = "images/eleven.svg",
     $smallLogo = "images/eleven-small.svg",
-    $logo = $(".logo");
+    $logo = $(".logo"),
+    $topBtn = $("#topBtn");
 
   $window.scroll(function () {
     // console.log(scrollY);
     if ($(this).scrollTop() > 100) {
+      $topBtn.show();
       if (!$mainHeader.hasClass("shrink")) {
         $mainHeader.addClass("shrink");
         changLogo($smallLogo);
       }
     } else {
+      $topBtn.hide();
       if ($mainHeader.hasClass("shrink")) {
         $mainHeader.removeClass("shrink");
         changLogo($defaultLogo);
@@ -28,11 +31,28 @@ $(document).ready(function () {
     }
   })   /* scroll */
 
+
+  /* 메뉴 로고 교체  */
   function changLogo(path) {
     $logo.fadeOut(500, function () {
       $logo.attr("src", path)
       $logo.fadeIn(500)
     })
   }
-
 }) /* ready end  */
+
+
+// 부드럽게 올라가기
+function topFunction() {
+  window.scrollTo(0, 0);
+  // html.scrollBehavior = "smooth";
+
+  /*   document.documentElement.style.scrollBehavior = "smooth";
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+   */
+
+
+}
+
+
---
+++ jQuery_16_shrinking-navbar_base-1111/js/topbt.js
@@ -0,0 +1,5 @@
+function topFunction() {
+  document.documentElement.style.scrollBehavior = "smooth";
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
