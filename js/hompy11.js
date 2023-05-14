43dbffd175788df932c8b83ff286c8150d506e44
sevenpia <sevenpia@naver.com>
2023-05-15 08:10:42 +0900

    all

--- js/hompy11.js
+++ js/hompy11.js
@@ -1,39 +1,52 @@
-// 제이쿼리로 작성
+/*  ########### 제이쿼리로 작성  ########### */
 
-/* $(function () {
+$(function () {
   $(".toggle").click(function () {
     $(".toggle").toggleClass("active")
+    $(".sidebar").toggleClass("active")
   })
-}) */
+  $(".menu li a, section").click(function () {
+    $(".sidebar, .toggle").removeClass("active")
+    // $(".toggle").removeClass("active")
+  })
+})
+
 
 /* ready end  */
 
-// 자바스크립트로 작성
 
-/* toggleMenu.addEventListener("click", function () {
-  // console.log(toggleMenu)
-  toggleMenu.classList.toggle("active");
-  sideBar.classList.toggle("active");
+/*  ########### 자바스크립트 함수로 작성  ########### */
 
-}); */
-
-
-// 자바스크립트 함수로 작성
-
-function toggleMenu() {
+/* function toggleMenu() {
   const toggleMenu = document.querySelector(".toggle");
   const sideBar = document.querySelector(".sidebar");
 
   toggleMenu.classList.toggle("active");
   sideBar.classList.toggle("active");
-}
+} */
 
-// 제이쿼리로 작성
-/* $(".toggle ").click(function () {
-  $(".sidebar").toggleClass("active")
+
+
+/*  ########### 자바스크립트로 작성  ==> 망함  ########### */
+/* 
+const toggleMenu = document.querySelector(".toggle");
+const sideBar = document.querySelector(".sidebar");
+const menuList = document.querySelectorAll(".sidebar ul.menu li a");
+console.log(menuList);
+
+toggleMenu.addEventListener("click", function () {
+  // console.log(toggleMenu)
+  toggleMenu.classList.toggle("active");
+  sideBar.classList.toggle("active");
+});
+menuList.addEventListener("click", function () {
+  toggleMenu.classList.remove("active");
+  sideBar.classList.remove("active");
 })
  */
 
+
+/* ###########  top button  ######## */
 
 $(function () {
   $(window).scroll(function () {
@@ -48,8 +61,4 @@ $(function () {
     window.scrollTo(0, 0);
   })
 
-  /*   $(".bottomBtn").click(function () {
-      window.scrollTo(0, 8000);
-    }) */
-
-}) 
+})
